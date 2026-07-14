6e6c69ebcba471723301740e50d1a24c5642a0ca
Omar Mahmoud <92801203+OmarMahmoudAhmed@users.noreply.github.com>
2026-07-14 09:12:39 +0000

    Update level card and add icons

--- src/components/home/LevelCard.jsx
+++ src/components/home/LevelCard.jsx
@@ -106,8 +106,8 @@ function LevelCard({ level }) {
         <img
           src={level.iconSrc}
           alt={level.nameAr}
-          width={50}
-          height={50}
+          width={100}
+          height={100}
           style={{ objectFit: 'contain' }}
         />
 
